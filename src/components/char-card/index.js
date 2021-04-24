356ad45242239330af83d0d4d74e42d6cd3585c5
João Balardin <joao_carpes@hotmail.com>
2021-04-24 16:56:18 -0300

    change lista chars

--- src/components/char-card/index.js
+++ src/components/char-card/index.js
@@ -3,14 +3,15 @@ import {Link} from 'react-router-dom';
 
 import './char-card.css';
 
-function CharCard({id, chars, tipo}){
+function CharCard({id, chars, level, tipo, skill}){
 
     return(
         <div className="col-md-12 col-sm-12">
- 
             <div className="card-body">
                 <p className="card-text text-justify">
-                    {chars} - {tipo} - <Link to={'/worlddetalhes/' + id} className="btn btn-sm btn-detalhes"> Detalhes</Link> 
+                    <h>Nick - Level - Classe - Skill</h>
+                    <br></br>
+                    {chars} - {level} - {tipo} - {skill} - <Link to={'/worlddetalhes/' + id} className="btn btn-sm btn-detalhes"> Detalhes</Link> 
                 </p>
 
             </div>
--- src/view/world-detalhes/index.js
+++ src/view/world-detalhes/index.js
@@ -7,142 +7,117 @@ import Navbar from "../../components/navbar/";
 import CharCard from "../../components/char-card";
 
 function WorldDetalhes(props) {
-  const [world, setWorld] = useState({});
-  const [chars, setChars] = useState([]);
-  const [pesquisa, setPesquisa] = useState("");
-  const usuarioLogado = useSelector((state) => state.usuarioEmail);
-  const [carregando, setCarregando] = useState(1);
-  const [excluido, setExcluido] = useState(0);
+    const [world, setWorld] = useState({});
+    const [chars, setChars] = useState([]);
+    const [pesquisa, setPesquisa] = useState("");
+    const usuarioLogado = useSelector((state) => state.usuarioEmail);
+    const [carregando, setCarregando] = useState(1);
+    const [excluido, setExcluido] = useState(0);
 
-  function remover() {
-    firebase
-      .firestore()
-      .collection("worlds")
-      .doc(props.match.params.id)
-      .delete()
-      .then(() => {
-        setExcluido(1);
-      });
-  }
-
-  useEffect(() => {
-    if (carregando) {
-      firebase
-        .firestore()
-        .collection("worlds")
-        .doc(props.match.params.id)
-        .get()
-        .then((resultado) => {
-          setWorld(resultado.data());
-          setCarregando(0);
-          queryPesquisa("");
+    function remover() {
+        firebase.firestore().collection("worlds").doc(props.match.params.id).delete().then(() => {
+            setExcluido(1);
         });
     }
-  }, []);
 
-  function executaPesquisa(e) {
-    queryPesquisa(e.target.value)
-  }
-
-  function queryPesquisa(texto){
-    let listachars = [];
-    firebase
-      .firestore()
-      .collection("worlds")
-      .doc(props.match.params.id)
-      .collection("char")
-      .get()
-      .then(async (resultado) => {
-        await resultado.forEach((doc) => {
-          if (pesquisa === "" || doc.data().char?.indexOf(pesquisa) >= 0) {
-            listachars.push({
-              id: doc.id,
-              ...doc.data(),
+    useEffect(() => {
+        if (carregando) {
+        firebase.firestore().collection("worlds").doc(props.match.params.id).get().then((resultado) => {
+            setWorld(resultado.data());
+            setCarregando(0);
+            queryPesquisa("");
             });
-          }
+        }
+    }, []);
+
+    function executaPesquisa(e) {
+        queryPesquisa(e.target.value)
+    }
+
+    function queryPesquisa(texto){
+        let listachars = [];
+        firebase.firestore().collection("worlds").doc(props.match.params.id).collection("char").get().then(async (resultado) => {
+            await resultado.forEach((doc) => {
+                if (doc.data().char.indexOf(pesquisa) >= 0) {
+                    listachars.push({
+                    id: doc.id,
+                    ...doc.data(),
+                    });
+                }
+            });
+            console.log(listachars)
+            setChars(listachars);
         });
-        console.log(listachars)
-        setChars(listachars);
-      });
-    setPesquisa(texto)
-  }
+        setPesquisa(texto)
+    }
 
-  console.log(chars);
-  return (
-    <>
-      <Navbar />
+    console.log(chars);
+    return (
+        <>
+        <Navbar />
 
-      <div className="row p-6 ">
-        <h2 className="mx-auto p-5">Mundos Criados</h2>
-        <input
-          onChange={executaPesquisa}
-          type="text"
-          className="form-control text-center"
-          placeholder="Pesquisar Mundo pelo nome..."
-        />
-      </div>
+        <div className="row p-6 ">
+            <h2 className="mx-auto p-5">Mundos Criados</h2>
+            <input onChange={executaPesquisa} type="text" className="form-control text-center" placeholder="Pesquisar Char pelo nome..."
+            />
+        </div>
 
-      <div className="row p-12">
-        <h3>Lista de Mundos:</h3>
-        {chars.map((item) => (
-          <CharCard
-            key={item.id}
-            id={item.id}
-            chars={item.char}
-            tipo={item.tipo}
-          />
-        ))}
-      </div>
+        <div className="row p-12">
+            <h3>Lista de Chars:</h3>
+            {
+                chars.map((item) => (<CharCard key={item.id} id={item.id} chars={item.char} level={item.level} tipo={item.tipo} skill={item.skill} />))
+            }
+        </div>
 
-      {excluido ? <Redirect to="/" /> : null}
+        {excluido ? <Redirect to="/" /> : null}
 
-      <div className="container-fluid">
-        {carregando ? (
-          <div className="row mt-5">
-            {" "}
-            <div className="spinner-border text-danger mx-auto" role="status">
-              <span className="sr-only"></span>
-            </div>{" "}
-          </div>
-        ) : (
-          <div>
-            <div className="row">
-              <h3 className="mx-auto mt-5 titulo">
-                <strong>{world.world}</strong>
-              </h3>
+        <div className="container-fluid">
+            {carregando ? (
+            <div className="row mt-5">
+                {" "}
+                <div className="spinner-border text-danger mx-auto" role="status">
+                <span className="sr-only"></span>
+                </div>{" "}
             </div>
+            ) : (
+            <div>
+                <div className="row">
+                <h3 className="mx-auto mt-5 titulo">
+                    <strong>{world.world}</strong>
+                </h3>
+                </div>
 
-            <div className="row mt-5 d-flex justify-content-around ">
-              <div className="col-md-3 col-sm-12 box-info p-3 my-2">
-                <i className="fas fa-ticket-alt fa-2x"></i>
-                <span className="mt-3">{world.tipo}</span>
-              </div>
+                <div className="row mt-5 d-flex justify-content-around ">
+                <div className="col-md-3 col-sm-12 box-info p-3 my-2">
+                    <i className="fas fa-ticket-alt fa-2x"></i>
+                    <span className="mt-3">{world.tipo}</span>
+                </div>
+                </div>
+
+                {usuarioLogado === world.usuario ? (
+                <Link
+                    to={`/editarworld/${props.match.params.id}`}
+                    type="button"
+                    className="btn btn-lg btn-block mt-3 mb-5 btn-cadastro"
+                >
+                    Editar Mundo{" "}
+                </Link>
+                ) : null}
+
+                {usuarioLogado === world.usuario ? (
+                <button
+                    onClick={remover}
+                    type="button"
+                    className="btn btn-lg btn-block mt-3 mb-5 btn-cadastro"
+                >
+                    Remover Mundo{" "}
+                </button>
+                ) : null}
             </div>
-
-            {usuarioLogado === world.usuario ? (
-              <Link
-                to={`/editarworld/${props.match.params.id}`}
-                type="button"
-                className="btn btn-lg btn-block mt-3 mb-5 btn-cadastro"
-              >
-                Editar Mundo{" "}
-              </Link>
-            ) : null}
-
-            {usuarioLogado === world.usuario ? (
-              <button
-                onClick={remover}
-                type="button"
-                className="btn btn-lg btn-block mt-3 mb-5 btn-cadastro"
-              >
-                Remover Mundo{" "}
-              </button>
-            ) : null}
-          </div>
-        )}
-      </div>
-    </>
-  );
+            )}
+        </div>
+        </>
+    );
 }
 
 export default WorldDetalhes;
